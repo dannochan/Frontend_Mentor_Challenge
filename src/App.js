ad61d5491fe2f5715271ac4750f8763bfa9ede32
Desheng Chen <desheng.chen@stud.uni-bamberg.de>
2023-10-21 13:24:39 +0200

    add validation for user data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,59 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [FnError, setFnError] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [LnError, setLnError] = useState();
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [pwError, setPwError] = useState("");
+
+  const firstNameChangeHandler = (event) => {
+    setFirstName(event.target.value.trim());
+  };
+  const lastNameChangeHandler = (event) => {
+    setLastName(event.target.value.trim());
+  };
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value.trim());
+  };
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value.trim());
+  };
+
+  const firstNameBlurHandler = () => {
+    if (firstName === "") {
+      setFnError("First Name cannot be empty");
+    } else if (firstName.length < 2) {
+      setFnError("Please enter a valid first Name");
+    }
+  };
+
+  const lastNameBlurHandler = () => {
+    if (lastName === "") {
+      setLnError("Last Name cannot be empty");
+    } else if (firstName.length < 2) {
+      setLnError("Please enter a valid last Name");
+    }
+  };
+  const emailBlurHandler = () => {
+    if (email === "") {
+      setEmailError("Email Address cannot be empty");
+    } else if (!email.includes("@")) {
+      setEmailError("Please enter a valid email address");
+    }
+  };
+  const passwordBlurHandler = () => {
+    if (password === "") {
+      setPwError("Password cannot be empty");
+    } else if (password.length <= 4) {
+      setPwError("Please Enter a valid password");
+    }
+  };
+
   return (
     <>
       <div className="app__signup app__container">
@@ -23,17 +76,49 @@ function App() {
           </div>
 
           <div className="app__signup-form app-flex">
-            <div>
-              <input name="firstname" type="text" placeholder="First Name" />
+            <div className={FnError ? "invalid" : ""}>
+              <input
+                name="firstname"
+                value={firstName}
+                type="text"
+                placeholder="First Name"
+                onChange={firstNameChangeHandler}
+                onBlur={firstNameBlurHandler}
+              />
+              {FnError && <p>{FnError}</p>}
             </div>
-            <div>
-              <input name="lastname" type="text" placeholder="Last Name" />
+            <div className={LnError ? "invalid" : ""}>
+              <input
+                name="lastname"
+                value={lastName}
+                type="text"
+                placeholder="Last Name"
+                onChange={lastNameChangeHandler}
+                onBlur={lastNameBlurHandler}
+              />
+              {LnError && <p>{LnError}</p>}
             </div>
-            <div>
-              <input name="email" type="email" placeholder="Email Address" />
+            <div className={emailError ? "invalid" : ""}>
+              <input
+                name="email"
+                value={email}
+                type="email"
+                placeholder="Email Address"
+                onChange={emailChangeHandler}
+                onBlur={emailBlurHandler}
+              />
+              {emailError && <p>{emailError}</p>}
             </div>
-            <div>
-              <input name="password" type="password" placeholder="Password" />
+            <div className={pwError ? "invalid" : ""}>
+              <input
+                name="password"
+                value={password}
+                type="password"
+                placeholder="Password"
+                onChange={passwordChangeHandler}
+                onBlur={passwordBlurHandler}
+              />
+              {pwError && <p>{pwError}</p>}
             </div>
 
             <div className="app__signup-btn">
