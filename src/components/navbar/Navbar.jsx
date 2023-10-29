26fce7af61ee8a18353b7bedd188764ea47dfd3c
dannochan <62658862+dannochan@users.noreply.github.com>
2023-10-29 17:09:11 +0100

    add contents and styles in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from "react";
-import style from "./Navbar.module.scss";
+import React, { useState } from "react";
+import "./Navbar.scss";
+import menu from "../../assets/icon-menu.svg";
+import close from "../../assets/icon-menu-close.svg";
+import logo from "../../assets/logo.svg";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-  return <div>Home New Popular Trending Categories</div>;
+  const [toggle, setToggle] = useState(false);
+  const toggleHandler = () => {
+    setToggle(!toggle);
+  };
+
+  const toggleMenu = (
+    <motion.div
+      whileInView={{ x: [300, 0] }}
+      transition={{ duration: 0.85, ease: "easeOut" }}
+    >
+      <img src={close} alt="closeMenu" onClick={toggleHandler} />
+      <nav>
+        <ul>
+          {["home", "new", "popular", "trending", "categories"].map((item) => (
+            <li key={{ item }}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </motion.div>
+  );
+
+  return (
+    <div className="app__navbar">
+      <div className="app__navbar_logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <div className="app__navbar-mobile">
+        <img src={menu} alt="menu" onClick={toggleHandler} />
+        {toggle && toggleMenu}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
