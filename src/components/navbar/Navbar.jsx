4c082d688067fa67bb3ed6f5c8d5c8d67f783abf
dannochan <62658862+dannochan@users.noreply.github.com>
2023-10-30 20:58:15 +0100

    add desktop navigation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,33 +13,40 @@ const Navbar = () => {
 
   const toggleMenu = (
     <motion.div
-      whileInView={{ x: [300, 0] }}
+      whileInView={{ x: [250, 0] }}
       transition={{ duration: 0.85, ease: "easeOut" }}
     >
       <img src={close} alt="closeMenu" onClick={toggleHandler} />
-      <nav>
-        <ul>
-          {["home", "new", "popular", "trending", "categories"].map((item) => (
-            <li key={{ item }}>
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+
+      <ul>
+        {["home", "new", "popular", "trending", "categories"].map((item) => (
+          <li key={{ item }}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </motion.div>
   );
 
   return (
-    <div className="app__navbar">
+    <nav className="app__navbar">
       <div className="app__navbar_logo">
         <img src={logo} alt="logo" />
       </div>
+
+      <ul className="app__navbar-links">
+        {["home", "new", "popular", "trending", "categories"].map((item) => (
+          <li key={{ item }}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
 
       <div className="app__navbar-mobile">
         <img src={menu} alt="menu" onClick={toggleHandler} />
         {toggle && toggleMenu}
       </div>
-    </div>
+    </nav>
   );
 };
 
